20e53d7a74fae71519e9f85191cdce7186b33afd
wlski <105810032+wlski@users.noreply.github.com>
2025-09-02 13:49:32 -0400

    more stuff

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -11,17 +11,31 @@ struct ContentView: View {
     @State private var message = "I Am A Programmer!"
     var body: some View {
         VStack {
+            Spacer()
+            
             Image(systemName: "swift")
                 .resizable()
                 .scaledToFit()
                 .foregroundStyle(.orange)
+                .frame(width: 200, height: 200)
             Text(message)
                 .font(.largeTitle)
-                .fontWeight(.heavy)
-                .foregroundStyle(.red)
-            Button("Click Me!") {
-                message = "Awesome!"
+                .fontWeight(.thin)
+                .foregroundStyle(.black)
+            
+            Spacer()
+            
+            HStack{
+                Button("Awesome!") {
+                    message = "Awesome!"
+                }
+                Button("Click Me!") {
+                    message = "Great!"
+                }
             }
+            .buttonStyle(.borderedProminent)
+            .font(.title2)
+            .tint(.orange)
         }
         .padding()
     }
